9d0ace6b74fef7174fb223be30fdb7d211512cdd
Pedro Bianchini de Quadros <pedrobiqua@outlook.com>
2024-04-12 10:56:23 -0300

    Melhorias

--- scripts/.ipynb_checkpoints/audio_processing-checkpoint.py
+++ scripts/.ipynb_checkpoints/audio_processing-checkpoint.py
@@ -10,28 +10,67 @@ class AudioProcessor:
     Classe responsavel pela processamento dos dados coletados atraves do audio
     """
     def __init__(self):
+        self._debug = False
         self.plotter = Plotter()
 
+    def enable_debug(self):
+        self._debug = True
+
+    def disable_debug(self):
+        self._debug = False
+#============================================================
+    def __remove_noise(self, y, multiple):
+        limiar = np.mean(np.abs(y)) * multiple
+        print(f"Limiar{limiar}")
+        indices = np.where(y < limiar)[0]
+        y = np.delete(y, indices)
+        print(len(y))
+        return y
+
+    def __separar_em_caixas(self, y, del_t):
+        
+        conj_dados = [y[i:i+del_t] for i in range(0, len(i), del_t)]
+        dados = []
+        for array in conj_dados:
+            fp = findpeaks(lookahead=1)
+            results = fp.fit(y)
+            tem_picos = (results['peak'] == 0).all()
+            if(tem_picos):
+                dados.append(1)
+            else:
+                dados.append(0)
+
+        return dados
+        
+
     def audio_para_onda_retangular(self, audio_path, multiple=0.1):
+        # Carrega os dados do audio.
         y, sr = librosa.load(audio_path, sr=None)
-        # Ao inves de usar limiar utilizar o find peaks do scipy.
-        # Para encontrar os picos vamos utilizar esse lib findpeaks
-        #================================================
-        # o arquivo de som já está em binário!
-        #================================================
+
+        # Utilizar o limiar para remover os ruidos
+        y = self.__remove_noise(y, multiple)
+
+        # Separa em caixas onde serão detectados os picos
         del_t = 100
+        __separar_em_caixas(y, del_t)
+
+        #===========================
+        """
+        del_t = len(y)//300
         fp = findpeaks(lookahead=int(del_t))
         results = fp.fit(y)
-        plot = fp.plot() #Bug com Gdk, vou ter que testar isso em um caderno
+        plot = fp.plot()
+        """
+        #===========================
 
         array_peaks = results['df']['peak'].astype(int).values
-        print(array_peaks)
-        print("\n")
-        
-        # limiar = np.mean(np.abs(y)) * multiple
-        # onda_retangular = (y > limiar).astype(float)
 
-        # self.plotter.mostrar_grafico(y, sr, limiar)
+        # Variavel para debug do código, para ativar usar a função enable_debug()
+        if self._debug:
+            print(results)
+            print("\n")
+            print(array_peaks)
+            print("\n")
 
         return array_peaks
 
--- scripts/audio_processing.py
+++ scripts/audio_processing.py
@@ -18,24 +18,50 @@ class AudioProcessor:
 
     def disable_debug(self):
         self._debug = False
+#============================================================
+    def __remove_noise(self, y, multiple):
+        limiar = np.mean(np.abs(y)) * multiple
+        print(f"Limiar{limiar}")
+        indices = np.where(y < limiar)[0]
+        y = np.delete(y, indices)
+        print(len(y))
+        return y
+
+    def __separar_em_caixas(self, y, del_t):
+        
+        conj_dados = [y[i:i+del_t] for i in range(0, len(i), del_t)]
+        dados = []
+        for array in conj_dados:
+            fp = findpeaks(lookahead=1)
+            results = fp.fit(y)
+            tem_picos = (results['peak'] == 0).all()
+            if(tem_picos):
+                dados.append(0)
+            else:
+                dados.append(1)
+
+        return dados
+        
 
     def audio_para_onda_retangular(self, audio_path, multiple=0.1):
+        # Carrega os dados do audio.
         y, sr = librosa.load(audio_path, sr=None)
-        # Ao inves de usar limiar utilizar o find peaks do scipy.
-        # Para encontrar os picos vamos utilizar esse lib findpeaks que já utiliza o scipy
 
+        # Utilizar o limiar para remover os ruidos
+        y = self.__remove_noise(y, multiple)
+
+        # Separa em caixas onde serão detectados os picos
+        del_t = 100
+        __separar_em_caixas(y, del_t)
+
+        #===========================
         """
-        Perguntas de pesquisa:
-            1- Eu não consigo estimar um contante de del_t pois os picos são aleatórios, 
-                ou seja não é possivel encontrar um intervalo plausivel para esse problema.
-            2- Tenho uma proposta, utilizar o metodo anterior de limiar para remover os 
-                ruidos montando um novo conjunto de dados e assim rodar o detector de picos.
-        """
-        del_t = 10000
-        # fp = findpeaks(method='caerus', params={'minperc':5, 'window':50}) # Essa opção não ficou legal
+        del_t = len(y)//300
         fp = findpeaks(lookahead=int(del_t))
         results = fp.fit(y)
-        plot = fp.plot() #Bug com Gdk, vou ter que testar isso em um caderno
+        plot = fp.plot()
+        """
+        #===========================
 
         array_peaks = results['df']['peak'].astype(int).values
 
@@ -45,13 +71,6 @@ class AudioProcessor:
             print("\n")
             print(array_peaks)
             print("\n")
-
-        #========================================#
-        # Metodo anterior
-        # limiar = np.mean(np.abs(y)) * multiple
-        # onda_retangular = (y > limiar).astype(float)
-        #========================================#
-        # self.plotter.mostrar_grafico(y, sr, limiar)
 
         return array_peaks
 
